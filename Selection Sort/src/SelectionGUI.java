61ebf82ef227648acd20261b7689fb1ce509780d
20ncherry <20ncherry@WA-248-9580.schools.westford-ma.gov>
2020-02-11 09:24:26 -0500

    2/11

---
+++ Selection Sort/src/SelectionGUI.java
@@ -0,0 +1,16 @@
+import BreezySwing.*;
+import javax.swing.*;
+public class SelectionGUI extends GBFrame{
+
+	
+	
+	public static void main(String[] args) {
+		JFrame frm = new SelectionGUI();
+		frm.setTitle("Ace Program");
+		frm.setSize(500, 425);
+		frm.setVisible(true);
+	}
+	
+	
+
+}
